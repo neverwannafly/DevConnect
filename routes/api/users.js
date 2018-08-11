fdcb8e2fe24caa5f0fc6c508f35aacccb3bfcbca
Shubham Anand <shubham_chess@live.com>
2018-08-11 22:29:07 +0530

    add headers to routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public route
 router.get('/test', (req, res) => {
     res.json({
         message: "Users Works!",
