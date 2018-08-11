f14aa06d431d8c82cf05d271f175034975b2acef
Shubham Anand <shubham_chess@live.com>
2018-08-11 23:44:21 +0530

    add post route to register users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
 const router = express.Router();
+
+// Load User Model
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -9,5 +15,44 @@ router.get('/test', (req, res) => {
         message: "Users Works!",
     });
 });
+
+// @route   GET api/users/register
+// @desc    Register user 
+// @access  Public route
+router.post('/register', (req, res) => {
+    User.findOne({
+        email: req.body.email
+    }).then(user => {
+        if (user) {
+            return res.status(400).json({
+                email: "Email Already Exists!"
+            });
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                avatar: gravatar.url(req.body.email, {
+                    d: 'mm', // Size
+                    r: 'pg', // Rating
+                    s: '200' // Default
+                }),
+                password: req.body.password,
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save().then(user => {
+                        res.json(user)
+                    }).catch(err => {
+                        console.log(err);
+                    });
+                });
+            });
+
+        }
+    })
+})
 
 module.exports = router
