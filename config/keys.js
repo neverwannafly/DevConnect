b417f2244c9afaad48d736a3de37c96afb08f293
Shubham Anand <shubham_chess@live.com>
2018-08-12 00:06:30 +0530

    add secret key

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://shubham:Shubham123@ds119572.mlab.com:19572/devconnect'
+    mongoURI: 'mongodb://shubham:Shubham123@ds119572.mlab.com:19572/devconnect',
+    secretKey: 'secret',
 }
