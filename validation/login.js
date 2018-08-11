d72d35139b25aee68c3a7d622a93d52c77b4bcb1
Shubham Anand <shubham_chess@live.com>
2018-08-12 02:47:06 +0530

    fix error

--- validation/login.js
+++ validation/login.js
@@ -1,7 +1,7 @@
 const Validator = require('validator');
 const isEmpty = require('./isEmpty');
 
-module.exports = function validateLoginInput(data) {
+module.exports = function validateProfileInput(data) {
     let errors = {};
 
     data.email = !isEmpty(data.email) ? data.email : '';
