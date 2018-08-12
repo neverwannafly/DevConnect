a97786b583985935362e2926e5b2436ab1fbb3b2
Shubham Anand <shubham_chess@live.com>
2018-08-12 14:33:28 +0530

    add line break

--- validation/register.js
+++ validation/register.js
@@ -2,6 +2,7 @@ const Validator = require('validator');
 const isEmpty = require('./isEmpty');
 
 module.exports = function validateRegisterInput(data) {
+
     let errors = {};
 
     data.name = !isEmpty(data.name) ? data.name : '';
