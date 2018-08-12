edf574c304cf4a347f8ca0f35931dd61ee45a4a6
Shubham Anand <shubham_chess@live.com>
2018-08-12 14:33:20 +0530

    add routes to add education and experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const validateProfileInput = require('./../../validation/profile');
+const validateExperienceInput = require('./../../validation/experience');
+const validateEducationInput = require('./../../validation/education');
 
 const router = express.Router();
 
@@ -170,6 +172,92 @@ router.post('/', passport.authenticate('jwt', {
         }
     });
 
+});
+
+// @route   POST api/profile/experience
+// @desc    Add experience to Profile
+// @access  Private
+router.post('/experience', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+
+    const {
+        errors,
+        isValid
+    } = validateExperienceInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status('400').json(errors);
+    }
+
+    Profile.findOne({
+        user: req.user.id
+    }).then(profile => {
+        if (!profile) {
+            errors.noprofile = 'Profile doesnt exist!';
+            return res.status('404').json(errors);
+        }
+        const newExp = {
+            title: req.body.title,
+            company: req.body.company,
+            location: req.body.location,
+            from: req.body.from,
+            to: req.body.to,
+            current: req.body.current,
+            description: req.body.description,
+        }
+
+        // Add to exp array
+        profile.experience.unshift(newExp);
+        profile.save().then(profile => {
+            res.json(profile);
+        });
+
+    });
+});
+
+// @route   POST api/profile/education
+// @desc    Add education to Profile
+// @access  Private
+router.post('/education', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+
+    const {
+        errors,
+        isValid
+    } = validateEducationInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status('400').json(errors);
+    }
+
+    Profile.findOne({
+        user: req.user.id
+    }).then(profile => {
+        if (!profile) {
+            errors.noprofile = 'Profile doesnt exist!';
+            return res.status('404').json(errors);
+        }
+        const newEdu = {
+            school: req.body.school,
+            degree: req.body.degree,
+            fieldofstudy: req.body.fieldofstudy,
+            from: req.body.from,
+            to: req.body.to,
+            current: req.body.current,
+            description: req.body.description,
+        }
+
+        // Add to exp array
+        profile.education.unshift(newEdu);
+        profile.save().then(profile => {
+            res.json(profile);
+        });
+
+    });
 });
 
 module.exports = router
