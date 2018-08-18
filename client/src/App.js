4035ce99677ed271fa9b39ec07919b4fcbeb6329
Shubham Anand <shubham_chess@live.com>
2018-08-18 22:24:35 +0530

    add private createProfile Route

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 import store from "./store";
 
 import "./App.css";
@@ -54,6 +55,7 @@ class App extends Component {
               <Route exact path="/login/" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard/" component={Dashboard} />
+                <PrivateRoute exact path="/create-profile/" component={CreateProfile} />
               </Switch>
             </div>
             <Footer />
