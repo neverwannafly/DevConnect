48212d2da13b483fa6313ff8e5a861953e97d2c6
Shubham Anand <shubham_chess@live.com>
2018-08-18 22:31:00 +0530

    add blueprint for select lists

---
+++ client/src/components/common/SelectListGroup.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import classnames from "classnames";
+import PropTypes from 'prop-types';
+
+const SelectListGroup = ({
+    options,
+    name, 
+    value,
+    error,
+    info,
+    onChange,
+}) => {
+
+    const selectOptions = options.map(option => (
+        <option key={option.label} value={option.value}>
+            {option.label}
+        </option>
+    ));
+
+    return (
+    <div className="form-group">
+        <select
+            className={classnames('form-control form-control-lg', {
+                'is-invalid': error
+            })}
+            name={name}
+            value={value}
+            onChange={onChange}
+        >
+            {selectOptions}
+        </select>
+        { info && <small className="form-text text-muted">{info}</small> }
+        {error && (<div className='invalid-feedback'>{error}</div>)}
+    </div>
+    )
+}
+
+SelectListGroup.propTypes = {
+    options: PropTypes.array.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    info: PropTypes.string,
+    error: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+}
+export default SelectListGroup;
