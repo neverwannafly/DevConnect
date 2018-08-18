d9d9133584ce185cacd819eda67fbfbf780544ca
Shubham Anand <shubham_chess@live.com>
2018-08-19 03:35:42 +0530

    add action to delete account

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from "./types";
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -43,3 +43,20 @@ export const clearCurrentProfile = () => {
         type: CLEAR_CURRENT_PROFILE,
     }
 };
+
+// Delete Account and Profile
+export const deleteAccount = () => dispatch => {
+    if (window.confirm("You're Deleting you Account! It cannot be undone")) {
+        axios.delete('/api/profile').then(res => {
+            dispatch({
+                type: SET_CURRENT_USER,
+                payload: {}
+            });
+        }).catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            })
+        });
+    }
+}
