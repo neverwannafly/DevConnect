ce2dd15a6ece65b431a7449d0f83a730f90620b1
Shubham Anand <shubham_chess@live.com>
2018-08-19 04:30:33 +0530

    create add-education route

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/add-experience";
+import AddEducation from "./components/add-credentials/add-education";
 import store from "./store";
 
 import "./App.css";
@@ -60,6 +61,7 @@ class App extends Component {
                 <PrivateRoute exact path="/create-profile/" component={CreateProfile} />
                 <PrivateRoute exact path="/edit-profile/" component={EditProfile} />
                 <PrivateRoute exact path="/add-experience/" component={AddExperience} />
+                <PrivateRoute exact path="/add-education/" component={AddEducation} />
               </Switch>
             </div>
             <Footer />
