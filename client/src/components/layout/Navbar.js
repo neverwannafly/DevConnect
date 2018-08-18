75081d2a8abf03070c0a465e17200d97ec86bf4d
Shubham Anand <shubham_chess@live.com>
2018-08-19 03:13:53 +0530

    add dashboard to the navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -24,12 +24,17 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
           <a 
             href="" 
             onClick={this.onLogoutClick} 
             className="nav-link">
             <img className="rounded-circle" src={user.avatar} style={{ width: '25px', marginRight: '5px' }} alt={user.name} title="You must have a gravatar connected to your email to display the image"/>
-            Logout
+            &nbsp;Logout
             </a>
         </li>
       </ul>
