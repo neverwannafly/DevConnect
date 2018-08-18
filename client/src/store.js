e6ea28edbe892cf6adb538ffc72a652672d41792
Shubham Anand <shubham_chess@live.com>
2018-08-19 02:23:05 +0530

    make chrome extensions only available if browser is chrome

--- client/src/store.js
+++ client/src/store.js
@@ -4,15 +4,29 @@ import thunk from "redux-thunk";
 
 const initialState = {};
 const middleware = [thunk];
+let store;
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        // to enable chrome extension
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+const isDevelopment = !!window.chrome && !!window.chrome.webstore;
+
+if (isDevelopment) {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware),
+            // to enable chrome extension
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    );
+} else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware),
+        )
+    );
+}
+
 
 export default store;
