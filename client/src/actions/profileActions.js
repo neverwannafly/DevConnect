e284f44f6d02995b9f8e940bde908473351579fd
Shubham Anand <shubham_chess@live.com>
2018-08-19 17:36:25 +0530

    add action to get all profiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,22 @@
 import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from "./types";
+import { GET_PROFILE, GET_PROFILES, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from "./types";
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+    dispatch(setProfileLoading());
+    axios.get('/api/profile/all').then(res => {
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data,
+        });
+    }).catch(err => {
+        dispatch({
+            type: GET_PROFILES,
+            payload: null,
+        });
+    });
+};
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -18,6 +34,7 @@ export const getCurrentProfile = () => dispatch => {
     });
 };
 
+// Create Profile
 export const createProfile = (profileData, history) => dispatch => {
     axios.post('/api/profile', profileData).then(result => {
         console.log(result);
