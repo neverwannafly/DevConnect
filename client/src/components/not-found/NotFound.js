1139d77a1102f9ba3f84e6f49843764610134b77
Shubham Anand <shubham_chess@live.com>
2018-08-19 20:28:37 +0530

    create a component for 404 errors

---
+++ client/src/components/not-found/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h4 className="display-4">Page Not Found</h4>
+      <p>Sorry, this page doesn't exist!!</p>
+    </div>
+  )
+}
