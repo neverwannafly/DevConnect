c72e79645c505ebfb14a7d4a67c0b98b0af888c3
Shubham Anand <shubham_chess@live.com>
2018-08-19 16:53:15 +0530

    add action to delete profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -54,6 +54,21 @@ export const addEducation = (eduData, history) => dispatch => {
     });
 }
 
+// Delete Experience
+export const deleteExperience = (id) => dispatch => {
+    axios.delete(`/api/profile/experience/${id}`).then(res => {
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data,
+        });
+    }).catch(err => {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data,
+        });
+    });
+}
+
 // Profile Loading
 export const setProfileLoading = () => {
     return {
